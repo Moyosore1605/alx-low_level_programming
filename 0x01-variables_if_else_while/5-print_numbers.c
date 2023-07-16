3c042fe18ede46245c92e68dc43727ad88c1a9ba
Moyosore1605 <moyosorejesuojo@gmail.com>
2023-07-16 09:34:31 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Return : always 0 (success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d\n", n);
+		n = n + 1;
+	}
+}
